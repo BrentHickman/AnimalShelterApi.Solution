07c142e4d8dd51e1555f1bc33ca5d5256f651a7e
BrentHickman <brenthickman@ymail.com>
2023-03-31 14:59:32 -0700

    DRY Replaced Cat and Dog models with Species property in Animal Model

--- AnimalShelterApi/Models/Animal.cs
+++ AnimalShelterApi/Models/Animal.cs
@@ -3,20 +3,23 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AnimalShelterApi.Models
 {
-  public class Cat
+  public class Animal
   {
-    public int CatId { get; set; }
+    public int AnimalId { get; set; }
+    [Required]
+    [StringLength(20)]
+    public string Species { get; set; }
     [Required]
     [StringLength(20)]
     public string Name { get; set; }
     [Required]
     [Range(0, 100, ErrorMessage = "Years must be between 0 and 100.")]
     public int AgeYears { get; set; }
-    [Required]
     [Range(0, 11, ErrorMessage = "Months must be between 0 and 11.")]
     public int AgeMonths { get; set; }
     [Required]
     public string Sex { get; set; }
+    [Required]
     [StringLength(30)]
     public string Breed { get; set; }
     [Required]
--- AnimalShelterApi/Models/AnimalShelterApiContext.cs
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -4,23 +4,19 @@ namespace AnimalShelterApi.Models
 {
   public class AnimalShelterApiContext : DbContext
   {
-    public DbSet<Dog> Dogs { get; set; }
-    public DbSet<Cat> Cats { get; set; }
+    public DbSet<Animal> Animals { get; set; }
     public AnimalShelterApiContext(DbContextOptions<AnimalShelterApiContext> options) : base(options)
     {
     }
 
         protected override void OnModelCreating(ModelBuilder builder)
     {
-      builder.Entity<Cat>()
+      builder.Entity<Animal>()
         .HasData(
-          new Cat { CatId = 1, Name = "Matilda", AgeYears = 0, AgeMonths = 6, Sex = "Female", Breed = "Domestic Short Hair", Weight = 3, Coloring = "Orange/White", Description = "Playful orange kitten Matilda came to us with her brother Rexie", DateAvailable = "4/10/2023" },
-          new Cat { CatId = 2, Name = "Rexie",AgeYears = 0, AgeMonths = 6, Sex = "Male", Breed = "Domestic Short Hair", Weight = 4, Coloring = "Gray/White", Description = "Mellow kitten Rexie came to us with his sister Matilda", DateAvailable = "4/10/2023"  }
-        );
-        builder.Entity<Dog>()
-        .HasData(
-          new Dog { DogId = 1, Name = "Danny", AgeYears = 2, AgeMonths = 6, Sex = "Male", Breed = "Boxer/Mix", Weight = 60, Coloring = "Brown/White", Description = "Fun guy Danny loves chasing bugs and taking naps", DateAvailable = "4/15/2023" },
-          new Dog { DogId = 2, Name = "Mary", AgeYears = 0, AgeMonths = 9, Sex = "Female", Breed = "Labrador", Weight = 35, Coloring = "Yellow", Description = "Mary is a sweet puppy who loves playing with kittens Matilda and Rexie", DateAvailable = "4/16/2023"  }
+          new Animal { AnimalId = 1, Species = "Cat", Name = "Matilda", AgeYears = 0, AgeMonths = 6, Sex = "Female", Breed = "Domestic Short Hair", Weight = 3, Coloring = "Orange/White", Description = "Playful orange kitten Matilda came to us with her brother Rexie", DateAvailable = "4/10/2023" },
+          new Animal { AnimalId = 2, Species = "Cat", Name = "Rexie",AgeYears = 0, AgeMonths = 6, Sex = "Male", Breed = "Domestic Short Hair", Weight = 4, Coloring = "Gray/White", Description = "Mellow kitten Rexie came to us with his sister Matilda", DateAvailable = "4/10/2023"  },
+          new Animal { AnimalId = 3,  Species = "Dog", Name = "Danny", AgeYears = 2, AgeMonths = 6, Sex = "Male", Breed = "Boxer/Mix", Weight = 60, Coloring = "Brown/White", Description = "Fun guy Danny loves chasing bugs and taking naps", DateAvailable = "4/15/2023" },
+          new Animal { AnimalId = 4, Species = "Dog", Name = "Mary", AgeYears = 0, AgeMonths = 9, Sex = "Female", Breed = "Labrador", Weight = 35, Coloring = "Yellow", Description = "Mary is a sweet puppy who loves playing with kittens Matilda and Rexie", DateAvailable = "4/16/2023"  }
         );
     }
   }
