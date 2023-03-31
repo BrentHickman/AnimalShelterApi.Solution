9e327af8fec3f6a2adf927993a87b1b19c12f465
BrentHickman <brenthickman@ymail.com>
2023-03-31 11:42:06 -0700

    Cat and Dog models plus Context.

---
+++ AnimalShelterApi/Models/Dog.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+
+namespace AnimalShelterApi.Models
+{
+  public class Dog
+  {
+    public int DogId { get; set; }
+    [Required]
+    [StringLength(20)]
+    public string Name { get; set; }
+    [Required]
+    [Range(0, 11, ErrorMessage = "Months must be between 0 and 11.")]
+    public int AgeMonths { get; set; }
+    [Required]
+    [Range(0, 100, ErrorMessage = "Years must be between 0 and 100.")]
+    public int AgeYears { get; set; }
+    [StringLength(30)]
+    public string Breed { get; set; }
+    [Required]
+    [StringLength(20)]
+    public string Coloring { get; set; }
+    public string Description { get; set; }
+    public string DateAvailable { get; set; }
+
+  }
+}
