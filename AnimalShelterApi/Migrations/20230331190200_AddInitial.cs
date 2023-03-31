86dd562ee87d9d88b42041648e1e279095fcd66e
BrentHickman <brenthickman@ymail.com>
2023-03-31 12:03:07 -0700

    initial migration looks good.

---
+++ AnimalShelterApi/Migrations/20230331190200_AddInitial.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnimalShelterApi.Migrations
+{
+    public partial class AddInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Cats",
+                columns: table => new
+                {
+                    CatId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    AgeYears = table.Column<int>(type: "int", nullable: false),
+                    AgeMonths = table.Column<int>(type: "int", nullable: false),
+                    Sex = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Breed = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Weight = table.Column<int>(type: "int", nullable: false),
+                    Coloring = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DateAvailable = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cats", x => x.CatId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Dogs",
+                columns: table => new
+                {
+                    DogId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    AgeYears = table.Column<int>(type: "int", nullable: false),
+                    AgeMonths = table.Column<int>(type: "int", nullable: false),
+                    Sex = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Breed = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Weight = table.Column<int>(type: "int", nullable: false),
+                    Coloring = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DateAvailable = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dogs", x => x.DogId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Cats",
+                columns: new[] { "CatId", "AgeMonths", "AgeYears", "Breed", "Coloring", "DateAvailable", "Description", "Name", "Sex", "Weight" },
+                values: new object[,]
+                {
+                    { 1, 6, 0, "Domestic Short Hair", "Orange/White", "4/10/2023", "Playful orange kitten Matilda came to us with her brother Rexie", "Matilda", "Female", 3 },
+                    { 2, 6, 0, "Domestic Short Hair", "Gray/White", "4/10/2023", "Mellow kitten Rexie came to us with his sister Matilda", "Rexie", "Male", 4 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Dogs",
+                columns: new[] { "DogId", "AgeMonths", "AgeYears", "Breed", "Coloring", "DateAvailable", "Description", "Name", "Sex", "Weight" },
+                values: new object[,]
+                {
+                    { 1, 6, 2, "Boxer/Mix", "Brown/White", "4/15/2023", "Fun guy Danny loves chasing bugs and taking naps", "Danny", "Male", 60 },
+                    { 2, 9, 0, "Labrador", "Yellow", "4/16/2023", "Mary is a sweet puppy who loves playing with kittens Matilda and Rexie", "Mary", "Female", 35 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cats");
+
+            migrationBuilder.DropTable(
+                name: "Dogs");
+        }
+    }
+}
